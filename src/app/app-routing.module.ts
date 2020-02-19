b7c47cef7004f4f36a885775a22897928021eab0
Tara Z. Manicsic <tzmanics@gmail.com>
2020-02-18 21:47:30 -0500

    cleans up old porject adds new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }, { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
